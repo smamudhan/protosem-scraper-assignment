d04106086511e36fc77a1354634606acdb373bb2
Amudhan <sm.amudhan@live.com>
2021-04-09 21:02:05 +0530

    :brush: Housekeeping

--- scraper.py
+++ scraper.py
@@ -10,12 +10,11 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 
 #Download chrome selenium webdriver and provide path to executable
-driver = webdriver.Chrome(r'C:\Users\IBK\Downloads\chromedriver_win32\chromedriver.exe', options=options)
+driver = webdriver.Chrome(r'C:\Users\smamu\Downloads\chromedriver_win32\chromedriver.exe', options=options)
 
 products = []
 variants = []
 prices = []
-# images = []
 desc = []
 count = 0
 get_count = 0
@@ -26,7 +25,6 @@ driver.implicitly_wait(10)
 
 while True:
     time.sleep(1)
-    #Workaround for lazy loaded images
     elements = driver.find_elements_by_class_name('_2kHMtA')
     for data in elements:
         coor = data.location_once_scrolled_into_view
@@ -34,13 +32,11 @@ while True:
     content = driver.page_source
     soup = BeautifulSoup(content, 'html.parser')
 
-    #Common classname that holds the data
     for a in soup.find_all('div', attrs={'class': '_2kHMtA'}):
         driver.implicitly_wait(10)
         name = a.find('div', attrs={'class': '_4rR01T'})
         price = a.find('div', attrs={'class': '_3I9_wc _27UcVY'})
         sale = a.find('div', attrs={'class': '_30jeq3 _1_WHN1'})
-#         image = a.find('div', attrs={'class': '_3BTv9X'}).find('img').get('src')
         des = a.find('ul', attrs={'class': '_1xgFaf'})
         try:
             products.append(name.text)
@@ -53,26 +49,12 @@ while True:
         except:
             test = ''.join(i for i in sale.text if i.isdigit())
             prices.append(test)
-#         try:
-#             split_data = name.text.split(' (')
-#             slug = split_data[0] + '_' + split_data[1].split(',')[0]
-#             urllib.request.urlretrieve(image, f"images\{slug}.jpg")
-#             images.append(slug)
-#         except:
-#             #If retrieved item was not lazy loaded, delete current entry and continue
-#             del prices[-1]
-#             del products[-1]
-#             del variants[-1]
-#             continue
         try:
             desc.append(des.get_text('\n', strip=True))
         except:
             desc.append("")
-    print(len(products))
-    #Scrolling to top of page
     driver.execute_script("window.scrollTo(0, 0);")
 
-    #Finding the next page button
     nxt_btn = driver.find_elements_by_class_name('_1LKTO3')
     if len(products) > 5:
         break
@@ -86,15 +68,6 @@ while True:
         continue
     else:
         break
-print("products length " , len(products))
-print(products)
-print("prices length " , len(prices))
-print("sales length " , len(sale))
-print("desc length " , len(desc))
-
-# df = pd.DataFrame({'Product Name': products, 'Variants': variants, 'Price': prices, 'Description': desc})
-# df.to_csv('new.csv', index=False, encoding='utf-8')
-
 
 def Convert(lst , v , p , d):
     res = []
@@ -104,18 +77,12 @@ def Convert(lst , v , p , d):
         res.append(dc)
         
     return res
-         
-# Driver code
-print(Convert(products , variants , prices , desc))
+
 result = Convert(products , variants , prices , desc)
 
 
 client = pymongo.MongoClient("mongodb+srv://ibk2510:deathbychocolate@cluster0.u4lph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.test
-# test
 db = client['Electronics']
-# define collection
 collection = db['smartphones']
-# sample data
 x = collection.insert_many(result)
-print(x)
